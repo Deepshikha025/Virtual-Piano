4cc26cc3b986556dd30a9e13c2cfb949cdd21f41
Deepshikha025 <91478605+Deepshikha025@users.noreply.github.com>
2021-11-20 13:36:05 +0530

    Add files via upload

---
+++ JavaScript/piano.js
@@ -0,0 +1,13 @@
+//select all the button divs
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => playSound(btn));
+});
+
+function playSound(btn){
+    const sound = document.getElementById(btn.dataset.key);
+    sound.currentTime=0;
+    sound.play();
+}
